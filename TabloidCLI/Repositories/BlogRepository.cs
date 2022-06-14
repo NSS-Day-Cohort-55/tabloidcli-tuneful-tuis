ea4752d0961f155e4380b06c0fef3c2e9b7616ed
Olivia Lind <eo.lind@gmail.com>
2022-06-14 13:58:57 -0500

    created Blog files and added ability to add blog

---
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI
+{
+    public class BlogRepository : DatabaseConnector, IRepository<Blog>
+    {
+        public BlogRepository(string connectionString) : base(connectionString) { }
+
+        public List<Blog> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Blog Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Insert(Blog blog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Blog (Title, Url )
+                                                     VALUES (@title, @url)";
+                    cmd.Parameters.AddWithValue("@title", blog.Title);
+                    cmd.Parameters.AddWithValue("@url", blog.Url);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Update(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ TabloidCLI/UserInterfaceManagers/BlogManager.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class BlogManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private BlogRepository _blogRepository;
+        private string _connectionString;
+
+        public BlogManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _blogRepository = new BlogRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Blog Menu");
+            Console.WriteLine(" 1) List Blogs");
+            Console.WriteLine(" 2) Blog Details");
+            Console.WriteLine(" 3) Add Blog");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    throw new NotImplementedException();
+                    //List();
+                    //return this;
+                case "2":
+                    throw new NotImplementedException();
+                    //Blog blog = Choose();
+                    //if (blog == null)
+                    //{
+                    //    return this;
+                    //}
+                    //else
+                    //{
+                    //    return new BlogDetailManager(this, _connectionString, blog.Id);
+                    //}
+                case "3":
+                    Add();
+                    return this;
+                case "4":
+                    throw new NotImplementedException();
+                    //Edit();
+                    //return this;
+                case "5":
+                    throw new NotImplementedException();
+                    //Remove();
+                    //return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            List<Blog> blogs = _blogRepository.GetAll();
+            foreach (Blog blog in blogs)
+            {
+                Console.WriteLine(blog);
+            }
+        }
+
+        private Blog Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please choose a Blog:";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Blog> blogs = _blogRepository.GetAll();
+
+            for (int i = 0; i < blogs.Count; i++)
+            {
+                Blog blog = blogs[i];
+                Console.WriteLine($" {i + 1}) {blog.Title}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return blogs[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
+        }
+
+        private void Add()
+        {
+            Console.WriteLine("New Blog");
+            Blog blog = new Blog();
+
+            Console.Write("Blog Title: ");
+            blog.Title = Console.ReadLine();
+
+            Console.Write("URL: ");
+            blog.Url = Console.ReadLine();
+
+            _blogRepository.Insert(blog);
+        }
+
+        private void Edit()
+        {
+
+            throw new NotImplementedException();
+            //Blog blogToEdit = Choose("Which blog would you like to edit?");
+            //if (blogToEdit == null)
+            //{
+            //    return;
+            //}
+
+            //Console.WriteLine();
+            //Console.Write("New title (blank to leave unchanged): ");
+            //string title = Console.ReadLine();
+            //if (!string.IsNullOrWhiteSpace(title))
+            //{
+            //    blogToEdit.Title = title;
+            //}
+            //Console.Write("New URL (blank to leave unchanged): ");
+            //string url = Console.ReadLine();
+            //if (!string.IsNullOrWhiteSpace(url))
+            //{
+            //    blogToEdit.Url = url;
+            //}
+
+            //_blogRepository.Update(blogToEdit);
+        }
+
+        private void Remove()
+        {
+            throw new NotImplementedException();
+            //Blog blogToDelete = Choose("Which blog would you like to remove?");
+            //if (blogToDelete != null)
+            //{
+            //    _blogRepository.Delete(blogToDelete.Id);
+            //}
+        }
+    }
+}
